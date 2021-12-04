ad4ed89032b927febe90be7a322a0596c8cc7958
Snowsock <iha001@uib.no>
2021-12-04 23:54:57 +0100

    December3 One star done

--- src/main/java/December3.java
+++ src/main/java/December3.java
@@ -6,16 +6,42 @@ import java.util.ArrayList;
 
 public class December3 implements AOC{
 
-    ArrayList<int []> input = new ArrayList<>();
+    ArrayList<String> input = new ArrayList<>();
+    int strLength;
 
-    public December3(String s) throws FileNotFoundException {
+    public December3(String s) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(s));
         String str;
+        while((str = br.readLine()) != null){
+            strLength = str.length();
+            input.add(str);
+        }
+
     }
 
     @Override
-    public int firstStar() throws IOException {
-        return 0;
+    public int firstStar(){
+        int bound = input.size() / 2;
+        String gamma = "";
+        String epsilon = "";
+        for(int i = 0 ; i < strLength ; ++ i){
+            int countOne = 0;
+            for(int j = 0; j < input.size(); ++j) {
+                char a = input.get(j).charAt(i);
+                if (a == '1') {
+                    countOne++;
+                }
+            }
+            if (countOne > bound) {
+                gamma += '1';
+                epsilon += '0';
+            } else {
+                gamma += '0';
+                epsilon += '1';
+            }
+        }
+
+        return Integer.parseInt(gamma,2)* Integer.parseInt(epsilon,2);
     }
 
     @Override
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,10 +5,12 @@ public class main {
     public static void main(String[] args) throws IOException {
         December1 d1 = new December1("/home/inge/git/AOC/src/main/resources/dec1");
         December2 d2 = new December2("/home/inge/git/AOC/src/main/resources/dec2");
+        December3 d3 = new December3("/home/inge/git/AOC/src/main/resources/dec3");
 
         System.out.println("December1, first star; " + d1.firstStar());
         System.out.println("December1, second star; " + d1.secondStar());
         System.out.println("December2, first star: "+ d2.firstStar());
         System.out.println("d2.secondStar() = " + d2.secondStar());
+        System.out.println("d3.firstStar() = " + d3.firstStar());
     }
 }
